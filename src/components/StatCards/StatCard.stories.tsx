11341a27fea185e4be4ec276f03a6683bb67ecd3
Viswath Senthilraj <viswathleon@gmail.com>
2025-02-07 12:01:46 +0530

    feat: add statcard story

---
+++ src/components/StatCards/StatCard.stories.tsx
@@ -0,0 +1,26 @@
+import { Meta } from '@storybook/react';
+import { ComponentProps } from 'react';
+import StatCard from '.';
+import { StatCardProps } from './types';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+type StoryProps = ComponentProps<typeof StatCard>;
+
+const meta: Meta<StoryProps> = {
+  component: StatCard,
+  title: 'Components/StatCard',
+};
+const Template = ({ icon, label, value }: StatCardProps) => (
+  <StatCard label={label} value={value} icon={icon} />
+);
+
+export const Default = Template.bind({});
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-ignore
+Default.args = {
+  icon: <ShoppingCartIcon />,
+  label: 'Total Product',
+  value: '9000',
+};
+
+export default meta;
