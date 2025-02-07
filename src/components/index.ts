a754616baeb790bca91cf72f51258d81ef0005af
Viswath Senthilraj <viswathleon@gmail.com>
2025-02-07 19:04:44 +0530

    chore: add index files for easy import

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export * from './Navbar';
+export * from './Button';
+export * from './ToggleSwitch';
+export * from './Table';
+export * from './StatCards';
+export { default as Modal } from './Modal';
+export * from './TextField';
