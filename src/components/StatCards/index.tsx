250996fd0067e7362c29fce2f6ab95336181ecc4
Viswath Senthilraj <viswathleon@gmail.com>
2025-02-07 12:01:14 +0530

    feat: add statcard component

---
+++ src/components/StatCards/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './StatCard.module.scss';
+import { StatCardProps } from './types';
+
+const StatCard: React.FC<StatCardProps> = ({ icon, label, value }) => {
+  return (
+    <div className={styles.statCard}>
+      {icon}
+      <div className={styles.content}>
+        <span className={styles.label}>{label}</span>
+        <span className={styles.value}>{value}</span>
+      </div>
+    </div>
+  );
+};
+
+export default StatCard;
---
+++ src/components/StatCards/types.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export type StatCardProps = {
+  icon: React.ReactNode;
+  label: string;
+  value: string | number;
+};
